15e8e4975bb01ca4573b3f7703a8efe515c10ec7
Konstantin Sukharev <poed@me.com>
2026-09-06 15:24:31 +0200

    Open new windows maximized

--- Xcopper/View/App.swift
+++ Xcopper/View/App.swift
@@ -10,6 +10,10 @@ struct XcopperApp: App {
 			EditorView(design: cfg.$document.design, clipboard: $clipboard)
 		}
 		.windowToolbarStyle(.unified)
+		.defaultWindowPlacement { _, context in
+			let screen = context.defaultDisplay.visibleRect
+			return WindowPlacement(screen.origin, size: screen.size)
+		}
 		.commands { MenuCommands() }
 	}
 }
